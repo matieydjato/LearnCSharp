42c4dfafda37d7fdc86607c5f36b4f14c84f6154
Djato <delpheandjato@gmail.com>
2024-09-10 22:50:53 +0000

    Boolean data type

---
+++ BooleanDataType/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BooleanDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isEnabled = true;
+            bool isMale = true;
+            isMale = false;
+            Console.WriteLine("isEnabled " + isEnabled); // True
+            Console.WriteLine("isMale " + isMale); // False
+        }
+    }
+}
