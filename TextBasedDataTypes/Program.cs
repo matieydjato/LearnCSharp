2bf0cd8f91d4b3b8b9c1402e56135de9c675e2a7
Djato <delpheandjato@gmail.com>
2024-09-10 16:36:57 +0000

    Text bases data types

---
+++ TextBasedDataTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TextBasedDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            char letter = 'A';
+            Console.WriteLine("Name " + name); // John
+            Console.Write("Letter " + letter); // A
+        }
+    }
+}
